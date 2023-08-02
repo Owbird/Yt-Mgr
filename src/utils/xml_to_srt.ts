9597808a0912c83115d1c7de65bcc2503c6a677b
owbird <owbirdphorccuo@gmail.com>
2023-08-02 02:37:35 +0000

    Fix (Caption): Time format fix

--- src/utils/xml_to_srt.ts
+++ src/utils/xml_to_srt.ts
@@ -1,4 +1,22 @@
 /**
+ * Formats the given time in seconds into a string representation of hours,
+ * minutes, and seconds.
+ *
+ * @param {number} timeInSeconds - The time to format in seconds.
+ * @return {string} The formatted time in the format "hh:mm:ss.SSS".
+ */
+function formatTime(timeInSeconds: number): string {
+  const hours = Math.floor(timeInSeconds / 3600)
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((timeInSeconds % 3600) / 60)
+    .toString()
+    .padStart(2, "0");
+  const seconds = (timeInSeconds % 60).toFixed(3).replace(".", ",");
+  return `${hours}:${minutes}:${seconds.padStart(6, "0")}`;
+}
+
+/**
  * Converts an XML string to an SRT string.
  *
  * @param {string} xml - The XML string to convert.
@@ -11,18 +29,13 @@ export default function convertXmlToSrt(xml: string): string {
 
   return Array.from(textNodes)
     .map((textNode, index) => {
-      const start = parseFloat(textNode.getAttribute("start")!)
-        .toFixed(3)
-        .replace(".", ",");
-      const duration = parseFloat(textNode.getAttribute("dur")!)
-        .toFixed(3)
-        .replace(".", ",");
-      const text = textNode.textContent!.trim().replace(/<\/?[^>]+(>|$)/g, "");
+      const startInSeconds = parseFloat(textNode.getAttribute("start")!);
+      const endInSeconds =
+        startInSeconds + parseFloat(textNode.getAttribute("dur")!);
+      const text = textNode.textContent!.trim().replace(/<\/?[^>]+>/g, "");
 
       return `${index + 1}
-${start} --> ${(parseFloat(start) + parseFloat(duration))
-        .toFixed(3)
-        .replace(".", ",")}
+${formatTime(startInSeconds)} --> ${formatTime(endInSeconds)}
 ${text}\n\n`;
     })
     .join("");
